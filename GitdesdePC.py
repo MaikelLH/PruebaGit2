8607aae3688fbc8de61fb89eadfa4d84fa5988e0
MaikelLH <105757536+MaikelLH@users.noreply.github.com>
2024-03-03 01:13:47 -0600

    Print en Python

---
+++ GitdesdePC.py
@@ -0,0 +1,4 @@
+#Practicando para crear un nuevo archivo entre Git y VS Code para GitHub
+print ("Mi nuevo repositorio en GitHub")
+print ("Ahora si puedo practicar Python desde donde sea 'o' ")
+       
